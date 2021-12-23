76de347c836bdd7ad759a07de444c4606d01a964
paulo-felipe <alparakleto@gmail.com>
2021-12-23 20:16:13 -0300

    test(models/book_spec.rb): add when create and update attributes with authors

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -30,6 +30,26 @@ RSpec.describe Book, type: :model do
         expect(book.quantity).to eq(27)
       end
     end
+
+    context 'with author' do
+      before do
+        book.authors.build([FactoryBot.attributes_for(:author),
+                            FactoryBot.attributes_for(:author)])
+        book.save
+      end
+
+      it 'counts 1 authors' do
+        expect(book.authors.count).to eq(2)
+      end
+
+      it 'has author first name' do
+        expect(book.authors.first.first_name).to be_truthy
+      end
+
+      it 'has author last name' do
+        expect(book.authors.first.last_name).to be_truthy
+      end
+    end
   end
 
   describe 'new attributes' do
@@ -58,6 +78,32 @@ RSpec.describe Book, type: :model do
 
       it 'has quantity' do
         expect(book.quantity).not_to eq(27)
+      end
+    end
+
+    context 'with author' do
+      before do
+        book.authors.build([{ first_name: Faker::Book.author.first,
+                              last_name: Faker::Book.author.last },
+                            { first_name: Faker::Book.author.first,
+                              last_name: Faker::Book.author.last }])
+        book.save
+      end
+
+      it 'changes author first name' do
+        author_first_name = book.authors.first.first_name
+        author_id = book.authors.first.id
+        book.authors.find(author_id).update(first_name: Faker::Book.author.first)
+
+        expect(book.authors.reload.first.first_name).not_to eq(author_first_name)
+      end
+
+      it 'changes author last name' do
+        author_last_name = book.authors.first.first_name
+        author_id = book.authors.first.id
+        book.authors.find(author_id).update(last_name: Faker::Book.author.first)
+
+        expect(book.authors.reload.first.last_name).not_to eq(author_last_name)
       end
     end
   end
