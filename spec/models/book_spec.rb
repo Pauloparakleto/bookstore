26fa561adde80798963df244923013a4abe8556d
paulo-felipe <alparakleto@gmail.com>
2021-12-23 18:06:03 -0300

    test(models/book_spec.rb): add when update attributes scenario

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   let!(:title) { Faker::Book.title }
   let!(:price) { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
-  let!(:book) { described_class.create(title: title, price: price, published: true) }
+  let!(:book) {
+    described_class.create(title: title, price: price, published: true,
+                           quantity: 27)
+  }
 
   describe 'attributes' do
     context 'when create' do
@@ -21,6 +24,40 @@ RSpec.describe Book, type: :model do
 
       it 'has published?' do
         expect(book.published?).to eq(true)
+      end
+
+      it 'has quantity' do
+        expect(book.quantity).to eq(27)
+      end
+    end
+  end
+
+  describe 'new attributes' do
+    context 'when update valid' do
+      before do
+        new_attributes = { title: Faker::Book.title, price: Faker::Number.number,
+                           published: false, quantity: Faker::Number.number.to_i }
+        book.update(new_attributes)
+      end
+
+      it 'has title' do
+        expect(book.title).not_to eq(title)
+      end
+
+      it 'has price' do
+        expect(book.price).not_to eq(price)
+      end
+
+      it 'has published' do
+        expect(book.published).not_to eq(true)
+      end
+
+      it 'has published?' do
+        expect(book.published?).not_to eq(true)
+      end
+
+      it 'has quantity' do
+        expect(book.quantity).not_to eq(27)
       end
     end
   end
