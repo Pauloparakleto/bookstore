1a39fe6ffdce3762a58eaab5ad400ba20ecbb1e6
paulo-felipe <alparakleto@gmail.com>
2021-12-23 23:09:09 -0300

    feat(books_controller.rb): add basic CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,44 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update]
+  before_action :build_book, only: [:create]
+
   def index
     @books = Book.published.map { |book| BooksPresenter.new(book) }
+  end
+
+  def show; end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    if @book.save
+      redirect_to book_path(@book), notice: 'Book was created!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @book.update(books_params)
+      redirect_to book_path(@book), notice: 'The Book was updated!'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:title, :price, :quantity)
+  end
+
+  def build_book
+    @book = Book.new(books_params)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
