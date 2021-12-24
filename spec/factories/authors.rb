abd454df879a9ccb61b2717a25813d8efa18a416
paulo-felipe <alparakleto@gmail.com>
2021-12-23 23:10:41 -0300

    chore(factories/authors.rb): add faker first and last names

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :author do
-    first_name { Faker::Book.author.first }
-    last_name { Faker::Book.author.last }
+    first_name { Faker::GreekPhilosophers.name }
+    last_name { Faker::Name.last_name }
   end
 end
