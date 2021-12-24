f6278ba4a486790f76e1f781b93201a4de32e7c5
paulo-felipe <alparakleto@gmail.com>
2021-12-23 21:24:29 -0300

    feat(models/book.rb): validates numericality of price and quantity

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :authors
 
   validates :title, :price, :quantity, presence: true
+  validates :price, :quantity, numericality: { greater_than_or_equal_to: 0 }
 
   scope :published, -> { where(published: true) }
 end
