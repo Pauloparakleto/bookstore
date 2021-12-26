a179aa9a5f13c398b852be30318e6a8548cf73ce
paulo-felipe <alparakleto@gmail.com>
2021-12-26 20:17:28 -0300

    refactor(models/order.rb): set total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,14 @@ class Order < ApplicationRecord
   has_many :items, dependent: :destroy
   accepts_nested_attributes_for :items
 
+  before_save :set_total_price
+
   def update(attributes)
     nil
+  end
+
+  def set_total_price
+    self.total_price = total
   end
 
   def total
