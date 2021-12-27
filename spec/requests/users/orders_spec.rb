4d428e782f937268e5a032d7e3510b4ea310a2de
paulo-felipe <alparakleto@gmail.com>
2021-12-27 20:21:48 -0300

    test(spec/requests/users/order_spec.rb): add book to cart post action

--- spec/requests/users/orders_spec.rb
+++ spec/requests/users/orders_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe '/users/orders' do
+RSpec.describe '/users/orders', type: :request do
   describe 'GETS cart' do
     it 'redirects to users books' do
       get cart_users_orders_path
       expect(response).to be_successful
     end
+  end
+
+  describe 'POSTs users/orders/add_book' do
+    let!(:book) { create_list(:book, 2) }
+
+    before do
+      post add_book_users_orders_path, params: { book: { id: book.first.id } }
+    end
+
+    it { is_expected.to redirect_to(users_books_path) }
   end
 end
