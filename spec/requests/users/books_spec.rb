92218ed4e4393c8b69112ae7208be08bf65464eb
paulo-felipe <alparakleto@gmail.com>
2021-12-28 09:55:12 -0300

    test(spec/requests/users/books_spec.rb): describe index and show

---
+++ spec/requests/users/books_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'users/books/', type: :request do
+
+  describe 'GETs Index' do
+    before do
+      FactoryBot.create_list(:book, 2)
+      get users_books_path
+    end
+
+    it { is_expected.to render_template('index') }
+  end
+
+  describe 'GETs Show' do
+    before do
+      book = FactoryBot.create(:book)
+      get users_book_path(book)
+    end
+
+    it { is_expected.to render_template('show') }
+  end
+end
