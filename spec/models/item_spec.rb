42f3413e8b0cf6ff227d4b6ba2236264790dd6aa
paulo-felipe <alparakleto@gmail.com>
2021-12-28 13:13:34 -0300

    test(spec/models/item_spec.rb): add validations to price and quantity

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'validations' do
+    subject(:item) { described_class.create(attributes_for(:item, book_id: book.id)) }
+
+    let!(:book) { create(:book) }
+
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+    it { is_expected.to validate_numericality_of(:quantity).is_greater_than_or_equal_to(1) }
+  end
+
   context 'when create invalid' do
     it 'has invalid quantity zero' do
       book = create(:book)
