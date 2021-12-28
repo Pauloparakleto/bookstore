badfe9ea8784152b8f0d4358ac83dbe7b09e557f
paulo-felipe <alparakleto@gmail.com>
2021-12-28 10:40:37 -0300

    chore(db/seeds.rb): add module Orders with items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
+# Module Authors
+puts 'Creating authors...'
 first_authors = FactoryBot.create_list(:author, 2)
 second_authors = FactoryBot.create_list(:author, 2)
-FactoryBot.create_list(:book, 3, published: true, authors: first_authors)
-FactoryBot.create_list(:book, 2, published: true, authors: second_authors)
-FactoryBot.create_list(:book, 2, published: false, authors: second_authors)
+
+# Module Books
+puts 'Creating Books'
+
+FactoryBot.create_list(:book, 3, published: true, authors: first_authors, quantity: 200)
+FactoryBot.create_list(:book, 2, published: true, authors: second_authors, quantity: 100)
+FactoryBot.create_list(:book, 2, published: false, authors: second_authors, quantity: 50)
+
+# Module Orders
+puts 'Creating Orders...'
+
+first_item = FactoryBot.create(:item, quantity: 2, book: Book.first)
+second_item = FactoryBot.create(:item, quantity: 3, book: Book.second)
+FactoryBot.create(:order, items: [first_item])
+FactoryBot.create(:order, items: [second_item])
