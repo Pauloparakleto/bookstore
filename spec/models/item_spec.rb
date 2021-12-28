629eae64c66354747267eeb46b08d2f525c95128
paulo-felipe <alparakleto@gmail.com>
2021-12-27 21:39:21 -0300

    fix(spec/models/item_spec.rb): add book to check quantity

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   context 'when create invalid' do
     it 'has invalid quantity zero' do
-      item = build(:item, quantity: 0)
+      book = create(:book)
+      item = build(:item, quantity: 0, book: book)
       item.save
       expect(item.errors.full_messages.first).to eq('Quantity must be greater than or equal to 1')
     end
