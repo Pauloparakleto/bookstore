9e47c9cbc58e483f76621eb2ba9f34568c7d0645
paulo-felipe <alparakleto@gmail.com>
2021-12-29 20:06:52 -0300

    test(helpers/application_helper_spec.rb): add name to block or unblock customer

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,16 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  it 'has a block path' do
-    user = create(:user)
-    path = block_customers_path(user)
-    expect(helper.path_to_block_or_unblock_customer(user)).to eq(path)
+  describe 'path to block or unblock' do
+    it 'has a block path' do
+      user = create(:user)
+      path = block_customers_path(user)
+      expect(helper.path_to_block_or_unblock_customer(user)).to eq(path)
+    end
+
+    it 'has an unblock path' do
+      user = create(:user)
+      user.blocked!
+      path = unblock_customers_path(user)
+      expect(helper.path_to_block_or_unblock_customer(user)).to eq(path)
+    end
   end
 
-  it 'has an unblock path' do
-    user = create(:user)
-    user.blocked!
-    path = unblock_customers_path(user)
-    expect(helper.path_to_block_or_unblock_customer(user)).to eq(path)
+  describe 'name to block or unblock' do
+    it 'has a block path' do
+      user = create(:user)
+      name = 'Block Customer'
+      expect(helper.name_to_block_or_unblock_customer(user)).to eq(name)
+    end
+
+    it 'has an unblock path' do
+      user = create(:user)
+      user.blocked!
+      name = 'Unblock Customer'
+      expect(helper.name_to_block_or_unblock_customer(user)).to eq(name)
+    end
   end
 end
