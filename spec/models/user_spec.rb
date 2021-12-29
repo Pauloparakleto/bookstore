f3587943f7bd16d3baf316ec0147e45ffe4c597a
paulo-felipe <alparakleto@gmail.com>
2021-12-29 12:04:21 -0300

    test(models/user_spec.rb): add order to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject(:user) { create(:user) }
 
+  let!(:book) { create(:book) }
+  let!(:order) { Order.new }
+
   describe 'validations' do
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
@@ -10,6 +13,21 @@ RSpec.describe User, type: :model do
   context 'when create' do
     it 'has email' do
       expect(user.email).to be_truthy
+    end
+  end
+
+  describe 'association to order' do
+    before do
+      order.items.build({ name: book.title, price: book.price, quantity: 2, book_id: book.id })
+      user.orders << order
+    end
+
+    it 'has order' do
+      expect(user.orders.first).to be_truthy
+    end
+
+    it 'has order item' do
+      expect(user.orders.first.items.first.name).to eq(book.title)
     end
   end
 end
