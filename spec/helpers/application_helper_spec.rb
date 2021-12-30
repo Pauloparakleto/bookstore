5bb1fc7d1fc262a43a9a488e3518708ccbf7a100
paulo-felipe <alparakleto@gmail.com>
2021-12-29 22:15:07 -0300

    fix(helpers/application_helper_spec.rb): paths

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe 'path to block or unblock' do
     it 'has a block path' do
       user = create(:user)
-      path = block_customers_path(user)
+      path = block_customers_path(id: user.id)
       expect(helper.path_to_block_or_unblock_customer(user)).to eq(path)
     end
 
     it 'has an unblock path' do
       user = create(:user)
       user.blocked!
-      path = unblock_customers_path(user)
+      path = unblock_customers_path(id: user.id)
       expect(helper.path_to_block_or_unblock_customer(user)).to eq(path)
     end
   end
