bf2d10dc9bc4b8d1776b518fa3bebf76ecc0bfcd
paulo-felipe <alparakleto@gmail.com>
2021-12-30 10:05:54 -0300

    feat(application_controller.rb): add after sign in and out to scope user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    users_root_path
+  end
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    users_root_path
+  end
 end
