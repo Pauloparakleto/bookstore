d992d958377f10f5e1c95dba3ae7c94b8712738e
paulo-felipe <alparakleto@gmail.com>
2021-12-30 11:40:56 -0300

    refactor(application_controller.rb): customize after sign in  with user and admin decisions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(_resource)
-    if _resource.is_a? User
-      users_root_path
-    elsif _resource.is_a? Admin
-      root_path
-    else
-      users_root_path
-    end
+  def after_sign_in_path_for(resource)
+    return root_path if resource.is_a? Admin
+
+    users_root_path
   end
 
   private
