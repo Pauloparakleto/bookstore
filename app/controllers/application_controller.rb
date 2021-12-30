62cde09565a44d2c8df408f9908229cb03041b07
paulo-felipe <alparakleto@gmail.com>
2021-12-30 10:13:57 -0300

    refactor(application_controller.rb): underscore unused arguments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     users_root_path
   end
 
   private
 
   # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     users_root_path
   end
 end
