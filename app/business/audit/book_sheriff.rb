604d4e9e809ce71b888abb39f648f4c279db83e6
paulo-felipe <alparakleto@gmail.com>
2021-12-31 20:14:42 -0300

    refactor(business/audit/book_sheriff_spec.rb): change class name and add check difference

---
+++ app/business/audit/book_sheriff.rb
@@ -0,0 +1,17 @@
+module Audit
+  class BookSheriff
+    attr_reader :admin, :book
+
+    def initialize(attributes, models)
+      @attributes = attributes
+      @admin = models.fetch(:admin)
+      @book = models.fetch(:book)
+    end
+
+    def check_difference
+      book_from_database = Book.find(book.id)
+      book_attributes_from_database = book_from_database.attributes.slice('title', 'quantity', 'price').symbolize_keys
+      difference = @attributes.delete_if { |_k, v| book_attributes_from_database.value?(v) }
+    end
+  end
+end
