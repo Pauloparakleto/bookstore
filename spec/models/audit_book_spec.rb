d5b6c8e43fce286b190b70073b7190e011bab3b5
paulo-felipe <alparakleto@gmail.com>
2021-12-31 12:02:23 -0300

    test(models/audit_book_spec.rb): add describe attributes and association belongs to admin and book

---
+++ spec/models/audit_book_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe AuditBook, type: :model do
+  subject(:audit_book) { described_class.new }
+
+  describe 'attributes' do
+    it 'book_id' do
+      expect(audit_book).to have_db_column(:book_id).with_options(null: false).of_type(:integer)
+    end
+
+    it 'admin_id' do
+      expect(audit_book).to have_db_column(:admin_id).with_options(null: false).of_type(:integer)
+    end
+
+    it 'title' do
+      expect(audit_book).to have_db_column(:title).of_type(:string)
+    end
+
+    it 'quantity' do
+      expect(audit_book).to have_db_column(:quantity).of_type(:integer)
+    end
+
+    it 'price' do
+      expect(audit_book).to have_db_column(:price).with_options(precision: 10, scale: 2).of_type(:decimal)
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to admin' do
+      expect(audit_book).to belong_to(:admin)
+    end
+
+    it 'belongs to book' do
+      expect(audit_book).to belong_to(:book)
+    end
+  end
+end
