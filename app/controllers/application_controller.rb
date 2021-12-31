12865ba5c790d41437145d397c5191b5c4af2de6
paulo-felipe <alparakleto@gmail.com>
2021-12-30 21:37:10 -0300

    feat(application_controller.rb): check blocked user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,18 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     return root_path if resource.is_a? Admin
 
+    check_blocked_user(resource)
     users_root_path
   end
 
   private
+
+  def check_blocked_user(resource)
+    return unless resource.blocked?
+
+    sign_out resource
+    flash[:alert] = 'You are blocked, please contact support'
+  end
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(_resource_or_scope)
