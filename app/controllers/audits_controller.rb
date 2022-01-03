8328230baa2cf539df7860a6ebd64e94279fdcbb
paulo-felipe <alparakleto@gmail.com>
2022-01-02 21:51:12 -0300

    feat(audits_controller.rb): authenticate admin!

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,4 +1,5 @@
 class AuditsController < ApplicationController
+  before_action :authenticate_admin!
   def index
     # TODO, presenter to index when field is empty 'no changes'
     @audit_books = AuditBook.all
