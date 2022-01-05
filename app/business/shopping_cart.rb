2cce3c1c20682f7cf3a7df4a9264702207e56f18
paulo-felipe <alparakleto@gmail.com>
2022-01-05 09:09:26 -0300

    feat(business/shopping_cart.rb): add future privates book quantity to hash, get quantity

--- app/business/shopping_cart.rb
+++ app/business/shopping_cart.rb
@@ -1,20 +1,27 @@
 class ShoppingCart
     attr_reader :book_ids
-    attr_accessor :book_unique_ids, :quantity
+    attr_accessor :book_unique_ids, :quantity, :book_quantity
 
     def initialize(array_of_ids)
         @book_ids = array_of_ids
         @book_unique_ids = book_ids.uniq
     end
 
-    def set_book_unique_ids
-       
-    end
-
-    def set_book_quantity
+    def set_quantity
         @quantity = []
         book_unique_ids.each do |id|
             quantity << book_ids.count(id)
         end
+    end
+
+    def book_quantity_to_hash
+        @book_quantity = {}
+        book_unique_ids.each_with_index do |book_id, index|
+            book_quantity[book_id] = quantity[index]
+        end
+    end
+
+    def get_quantity(book_id)
+        book_quantity.fetch(book_id, nil)
     end
 end
