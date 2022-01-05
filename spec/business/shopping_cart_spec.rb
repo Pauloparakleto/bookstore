a475305a54635a90e59a78d79dee06ac9c6aa9e6
paulo-felipe <alparakleto@gmail.com>
2022-01-05 19:17:11 -0300

    refactor(shopping_cart_spec.rb): remove testing private methods and add check an intance of order on initialize

--- spec/business/shopping_cart_spec.rb
+++ spec/business/shopping_cart_spec.rb
@@ -16,91 +16,60 @@ RSpec.describe ShoppingCart do
         it 'has unique ids' do
             expect(cart.book_unique_ids).to eq([books.first.id, books.second.id, books.third.id])
         end
-
-        it 'has hash with book quantity pair' do
-          cart.set_quantity
-          expect(cart.quantity).to eq([1, 2, 1])
-        end
-
-        it 'has hash of book quantity pair' do
-            cart.set_quantity
-            cart.book_quantity_to_hash
-            expect(cart.book_quantity).to eq({books.first.id=>1, books.second.id=>2, books.third.id=>1})
-        end
-
-        it 'gets 2 for second book quantity' do
-            cart.set_quantity
-            cart.book_quantity_to_hash
-            expect(cart.get_quantity(books.second.id)).to eq(2)
+        
+        it 'has an instance of order' do
+            expect(cart.order.is_a?(Order)).to be_truthy
         end
     end
 
     context 'when build items' do
       describe 'items quantity' do
         it 'has first item quantity 1' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
-          cart.build_order
+         cart.build_order
           expect(cart.order.items.first.quantity).to eq(1)
         end
         
         it 'has second item quantity 2' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
-          cart.build_order
+         cart.build_order
           expect(cart.order.items.second.quantity).to eq(2)
         end
         
         it 'has third item quantity 1' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
-          cart.build_order
+         cart.build_order
           expect(cart.order.items.third.quantity).to eq(1)
         end
       end
       
       describe 'items price' do
         it 'is equal book first price' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
           cart.build_order
-          expect(cart.order.items.first.price).to eq(books.first.price)
+          expect(cart.order.items.first.price.to_d).to eq(books.first.price.to_d)
         end
         
         it 'is equal book second price' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
           cart.build_order
-          expect(cart.order.items.second.price).to eq(books.second.price)
+          expect(cart.order.items.second.price.to_d).to eq(books.second.price.to_d)
         end
         
         it 'is equal book third price' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
           cart.build_order
-          expect(cart.order.items.third.price).to eq(books.third.price)
+          expect(cart.order.items.third.price.to_d).to eq(books.third.price.to_d)
         end
       end
       
       describe 'items name' do
         it 'is equal book first title' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
-          cart.build_order
+         cart.build_order
           expect(cart.order.items.first.name).to eq(books.first.title)
         end
         
         it 'is equal book second title' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
-          cart.build_order
+         cart.build_order
           expect(cart.order.items.second.name).to eq(books.second.title)
         end
         
         it 'is equal book third title' do
-          cart.set_quantity
-          cart.book_quantity_to_hash
-          cart.build_order
+         cart.build_order
           expect(cart.order.items.third.name).to eq(books.third.title)
         end
       end
