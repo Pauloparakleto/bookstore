f20b83ba96331096fe71922955f6447336eb5d60
paulo-felipe <alparakleto@gmail.com>
2022-01-05 09:04:26 -0300

    test(spec/shopiing_cart.rb): expects to the future private methods

--- spec/business/shopping_cart_spec.rb
+++ spec/business/shopping_cart_spec.rb
@@ -18,8 +18,25 @@ RSpec.describe ShoppingCart do
         end
 
         it 'has hash with book quantity pair' do
-          cart.set_book_quantity
+          cart.set_quantity
           expect(cart.quantity).to eq([1, 2, 1])
+        end
+
+        it 'has hash of book quantity pair' do
+            cart.set_quantity
+            cart.book_quantity_to_hash
+            expect(cart.book_quantity).to eq({books.first.id=>1, books.second.id=>2, books.third.id=>1})
+        end
+
+        it 'gets 2 for second book quantity' do
+            cart.set_quantity
+            cart.book_quantity_to_hash
+            expect(cart.get_quantity(books.second.id)).to eq(2)
+        end
+    end
+
+    context 'when build items' do
+        it 'has first item quantity 1' do
         end
     end
   end
