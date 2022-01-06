0451471c2286144fb94b84bea0b967a01da36936
Felipe <alparakleto@gmail.com>
2022-01-06 00:08:49 -0300

    refactor(shopiing_cart.rb): dolve offenses

--- app/business/shopping_cart.rb
+++ app/business/shopping_cart.rb
@@ -1,45 +1,45 @@
 class ShoppingCart
-    attr_reader :book_ids
-    attr_accessor :book_unique_ids, :quantity, :book_quantity, :order
+  attr_reader :book_ids
+  attr_accessor :book_unique_ids, :quantity, :book_quantity, :order
 
-    def initialize(array_of_ids)
-        @book_ids = array_of_ids
-        @book_unique_ids = book_ids.uniq
-        @order = Order.new
+  def initialize(array_of_ids)
+    @book_ids = array_of_ids
+    @book_unique_ids = book_ids.uniq
+    @order = Order.new
+  end
+
+  def build_order
+    set_quantity
+    book_quantity_to_hash
+    add_items_to_order
+    order
+  end
+
+  private
+
+  def add_items_to_order
+    book_quantity_to_hash.each do |id, quantity|
+      book = Book.find(id)
+      quantity = get_quantity(id)
+      order.items.build(name: book.title, price: book.price, quantity: quantity, book_id: id)
     end
+  end
 
-    def build_order
-        set_quantity
-        book_quantity_to_hash
-        add_items_to_order
-        order
+  def set_quantity
+    @quantity = []
+    book_unique_ids.each do |id|
+      quantity << book_ids.count(id)
     end
+  end
 
-    private
-
-    def add_items_to_order
-        book_quantity_to_hash.each do |id, quantity|
-            book = Book.find(id)
-            quantity = get_quantity(id)
-            order.items.build(name: book.title, price: book.price, quantity: quantity, book_id: id)
-        end
+  def book_quantity_to_hash
+    @book_quantity = {}
+    book_unique_ids.each_with_index do |book_id, index|
+      book_quantity[book_id] = quantity[index]
     end
+  end
 
-    def set_quantity
-        @quantity = []
-        book_unique_ids.each do |id|
-            quantity << book_ids.count(id)
-        end
-    end
-
-    def book_quantity_to_hash
-        @book_quantity = {}
-        book_unique_ids.each_with_index do |book_id, index|
-            book_quantity[book_id] = quantity[index]
-        end
-    end
-
-    def get_quantity(book_id)
-        book_quantity.fetch(book_id, nil)
-    end
+  def get_quantity(book_id)
+    book_quantity.fetch(book_id, nil)
+  end
 end
